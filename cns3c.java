a414b505eeda5fe41ec1202265fefc4e2d538b0d
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-03-07 19:42:14 +0530

    Update cns3c.java

--- cns3c.java
+++ cns3c.java
@@ -1,97 +1,47 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    static float[][] key = new float[3][3];
-    static float[][] inverseKey = new float[3][3];
-    static float[][] message = new float[3][1];
-    static float[][] encrypted = new float[3][1];
-    static float[][] decrypted = new float[3][1];
-    static Scanner scanner = new Scanner(System.in);
+    private static final int N = 2;
+    private static int[][] key = new int[N][N], invKey = new int[N][N];
 
     public static void main(String[] args) {
-        getInput();
-        encryptMessage();
-        System.out.print("\nEncrypted string is: ");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((char) (encrypted[i][0] % 26 + 97));
-        }
-        calculateInverse();
-        decryptMessage();
-        System.out.print("\nDecrypted string is: ");
-        for (int i = 0; i < 3; i++) {
-            System.out.print((char) (decrypted[i][0] % 26 + 97));
-        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 2x2 Key Matrix:");
+        for (int i = 0; i < N; i++) for (int j = 0; j < N; j++) key[i][j] = sc.nextInt();
+
+        System.out.println("Enter plaintext:");
+        String text = sc.next().toUpperCase();
+        if (text.length() % 2 != 0) text += "X";
+        sc.close();
+
+        String cipher = encrypt(text);
+        System.out.println("Ciphertext: " + cipher);
+
+        System.out.println(calcInvKey() ? "Decrypted text: " + decrypt(cipher) : "Key Matrix is not invertible. Exiting.");
     }
 
-    private static void getInput() {
-        System.out.println("Enter a 3x3 key matrix (must be invertible): ");
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                key[i][j] = scanner.nextFloat();
+    private static String encrypt(String text) { return process(text, key); }
+    private static String decrypt(String text) { return process(text, invKey); }
 
-        System.out.print("\nEnter a 3-letter string: ");
-        String msg = scanner.next();
-        for (int i = 0; i < 3; i++)
-            message[i][0] = msg.charAt(i) - 97;
+    private static String process(String text, int[][] mat) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < text.length(); i += N) {
+            int[] pair = {text.charAt(i) - 'A', text.charAt(i + 1) - 'A'}, resPair = new int[N];
+            for (int j = 0; j < N; j++)
+                for (int k = 0; k < N; k++) resPair[j] = (resPair[j] + mat[j][k] * pair[k]) % 26;
+            res.append((char) (resPair[0] + 'A')).append((char) (resPair[1] + 'A'));
+        }
+        return res.toString();
     }
 
-    private static void encryptMessage() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 1; j++) {
-                encrypted[i][j] = 0;
-                for (int k = 0; k < 3; k++) {
-                    encrypted[i][j] += key[i][k] * message[k][j];
-                }
-            }
-        }
-    }
-
-    private static void calculateInverse() {
-        float temp;
-        float[][] tempKey = new float[3][3];
-        for (int i = 0; i < 3; i++)
-            System.arraycopy(key[i], 0, tempKey[i], 0, 3);
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                inverseKey[i][j] = (i == j) ? 1 : 0;
-            }
-        }
-
-        for (int i = 0; i < 3; i++) {
-            temp = tempKey[i][i];
-            for (int j = 0; j < 3; j++) {
-                tempKey[i][j] /= temp;
-                inverseKey[i][j] /= temp;
-            }
-            for (int k = 0; k < 3; k++) {
-                if (i != k) {
-                    temp = tempKey[k][i];
-                    for (int j = 0; j < 3; j++) {
-                        tempKey[k][j] -= tempKey[i][j] * temp;
-                        inverseKey[k][j] -= inverseKey[i][j] * temp;
-                    }
-                }
-            }
-        }
-
-        System.out.println("\nInverse Matrix is:");
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(inverseKey[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    private static void decryptMessage() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 1; j++) {
-                decrypted[i][j] = 0;
-                for (int k = 0; k < 3; k++) {
-                    decrypted[i][j] += inverseKey[i][k] * encrypted[k][j];
-                }
-            }
-        }
+    private static boolean calcInvKey() {
+        int det = (key[0][0] * key[1][1] - key[0][1] * key[1][0] + 26) % 26, detInv = -1;
+        for (int i = 1; i < 26; i++) if ((det * i) % 26 == 1) { detInv = i; break; }
+        if (detInv == -1) return false;
+        invKey[0][0] = key[1][1] * detInv % 26;
+        invKey[0][1] = (-key[0][1] * detInv % 26 + 26) % 26;
+        invKey[1][0] = (-key[1][0] * detInv % 26 + 26) % 26;
+        invKey[1][1] = key[0][0] * detInv % 26;
+        return true;
     }
 }
