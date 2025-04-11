4fe90145b2fabd195218cad5fbe3cb5c1095495d
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-04-11 10:55:36 +0530

    Update cns6.java

--- cns6.java
+++ cns6.java
@@ -1,33 +1,24 @@
-import javax.crypto.*;
-import javax.crypto.spec.*;
-import java.util.Base64;
+/*Write a C/JAVA program to implement the Rijndael algorithm logic.*/
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Accept input message from user
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter message to encrypt: ");
-        String message = scanner.nextLine();
-
-        // Generate key
-        KeyGenerator kgen = KeyGenerator.getInstance("AES");
-        kgen.init(128);
-        SecretKey skey = kgen.generateKey();
-
-        // Encrypt message
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the string to encrypt: ");
+        String input = sc.nextLine();
+        String key = "1234567812345678";
+        SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "AES");
         Cipher cipher = Cipher.getInstance("AES");
+
         cipher.init(Cipher.ENCRYPT_MODE, skey);
-        byte[] encrypted = cipher.doFinal(message.getBytes());
-        String encryptedString = Base64.getEncoder().encodeToString(encrypted);
-        System.out.println("Encrypted: " + encryptedString);
+        byte[] enc = cipher.doFinal(input.getBytes());
+        String encrypted = new String(java.util.Base64.getEncoder().encode(enc));
+        System.out.println("encrypted text: " + encrypted);
 
-        // Decrypt message
         cipher.init(Cipher.DECRYPT_MODE, skey);
-        byte[] decrypted = cipher.doFinal(Base64.getDecoder().decode(encryptedString));
-        String decryptedMessage = new String(decrypted);
-        System.out.println("Decrypted: " + decryptedMessage);
-
-        scanner.close();
+        byte[] dec = cipher.doFinal(java.util.Base64.getDecoder().decode(encrypted));
+        System.out.println("decrypted text: " + new String(dec));
     }
 }
