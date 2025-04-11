d1f26b552b7401b5a3e84c5504d929008dee2f2f
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-04-11 10:52:21 +0530

    Update cns5.java

--- cns5.java
+++ cns5.java
@@ -1,41 +1,28 @@
-import java.io.*;
-import java.security.Key;
+/*Write a C/JAVA program to implement the BlowFish algorithm logic.*/
+
 import javax.crypto.Cipher;
-import javax.crypto.CipherOutputStream;
-import javax.crypto.KeyGenerator;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try {
-            // Generate a Blowfish key
-            KeyGenerator keyGenerator = KeyGenerator.getInstance("Blowfish");
-            keyGenerator.init(128); // Key size
-            Key secretKey = keyGenerator.generateKey();
-
-            // Initialize the cipher for encryption
-            Cipher cipher = Cipher.getInstance("Blowfish/CFB/NoPadding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-
-            // Read the input file
-            FileInputStream fin = new FileInputStream("inputFile.txt");
-            FileOutputStream fout = new FileOutputStream("outputFile.txt");
-            CipherOutputStream cout = new CipherOutputStream(fout, cipher);
-
-            // Read from the input file and write to the output file
-            int input;
-            while ((input = fin.read()) != -1) {
-                cout.write(input);
-            }
-
-            // Close all streams
-            fin.close();
-            cout.close();
-            fout.close();
-
-            System.out.println("File encrypted successfully!");
-
-        } catch (Exception e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter text to encrypt:");
+        String input = sc.nextLine();
+        System.out.println("orginal text:" + input);
+        String key = "sneha";
+        SecretKeySpec skey = new SecretKeySpec(key.getBytes(), "Blowfish");
+        Cipher cipher = Cipher.getInstance("Blowfish");
+        cipher.init(Cipher.ENCRYPT_MODE, skey);
+        byte[] enc = cipher.doFinal(input.getBytes());
+        System.out.print("encrypted:[");
+        for (int i = 0; i < enc.length; i++) {
+            System.out.print(enc[i]);
+            if (i < enc.length - 1) System.out.print(",");
         }
+        System.out.println("]");
+        cipher.init(Cipher.DECRYPT_MODE, skey);
+        byte[] dec = cipher.doFinal(enc);
+        System.out.println("decrypted:" + new String(dec));
     }
 }
