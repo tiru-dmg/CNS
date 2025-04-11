d2e6e3a3ea30e967fcd731a3ecb8c5810c8f32c7
Gopi Tirumala <141766397+tiru-dmg@users.noreply.github.com>
2025-04-11 14:42:09 +0530

    Update cns2.c

--- cns2.c
+++ cns2.c
@@ -1,11 +1,17 @@
+/*Write a C program that contains a string (char pointer) with a value \Hello
+World’. The program should AND or and XOR each character in this string with
+127 and display the result.*/
+
 #include <stdio.h>
-#include <string.h>
 int main() {
-    char str[] = "Hello World", str1[11], str2[11];
-    int i, len = strlen(str);
-    for (i = 0; i < len; i++) str1[i] = str[i] & 127, printf("%c", str1[i]);
-    printf("\n");
-    for (i = 0; i < len; i++) str2[i] = str[i] ^ 127, printf("%c", str2[i]);
+    char str[] = "Hello World";
+    int i;
+    printf("AND: ");
+    for (i = 0; str[i]; i++) printf("%c", (str[i] & 127) >= 32 && (str[i] & 127) <= 126 ? str[i] & 127 : '?');
+    printf("\nOR: ");
+    for (i = 0; str[i]; i++) printf("%c", (str[i] | 127) >= 32 && (str[i] | 127) <= 126 ? str[i] | 127 : '^');
+    printf("\nXOR: ");
+    for (i = 0; str[i]; i++) printf("%c", (str[i] ^ 127) >= 32 && (str[i] ^ 127) <= 126 ? str[i] ^ 127 : '&');
     printf("\n");
     return 0;
 }
